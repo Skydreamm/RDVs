b962c034e061b582569e4b6b5dae72826ad77901
Skydreamm <55774603+Skydreamm@users.noreply.github.com>
2026-08-24 14:29:08 +0200

    Add authuser parameter to Google Script URLs

--- data/config.js
+++ data/config.js
@@ -2,12 +2,12 @@
 window.APP_CONFIG = {
   adminEmail: "lounis.said@saint-quentin.fr",
   enableMailNotification: true,
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbw4JZ12EYTM8QfMa5dT0WTnNRLczi4hF3u2e9rprLlsiXNhe1WLwuJP2XrHRwaDAaks/exec",
-  syncDataUrl: "https://script.google.com/macros/s/AKfycbw4JZ12EYTM8QfMa5dT0WTnNRLczi4hF3u2e9rprLlsiXNhe1WLwuJP2XrHRwaDAaks/exec",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbw4JZ12EYTM8QfMa5dT0WTnNRLczi4hF3u2e9rprLlsiXNhe1WLwuJP2XrHRwaDAaks/exec?authuser=0",
+  syncDataUrl: "https://script.google.com/macros/s/AKfycbw4JZ12EYTM8QfMa5dT0WTnNRLczi4hF3u2e9rprLlsiXNhe1WLwuJP2XrHRwaDAaks/exec?authuser=0",
 
   // Service séparé pour les flyers publics.
   // Déploie google-apps-script-activities.gs en Application Web puis colle ici l'URL qui finit par /exec.
-  activitiesScriptUrl: "https://script.google.com/macros/s/AKfycbxT1U0BkEwIgRsOzT7t5jBXoBWRn-8diWkbMOwrPv_s37NC9-ZN9mPgmdKUf46nNDNa/exec",
+  activitiesScriptUrl: "https://script.google.com/macros/s/AKfycbxT1U0BkEwIgRsOzT7t5jBXoBWRn-8diWkbMOwrPv_s37NC9-ZN9mPgmdKUf46nNDNa/exec?authuser=0",
 
   refreshSeconds: 1
 };
